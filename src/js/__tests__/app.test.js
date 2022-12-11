ee60df6505ba3c0c3bf99028d6ea7c32861c338f
Sancho Rodriges <softilla@ya.ru>
2022-12-11 16:27:59 +0300

    each

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,11 +1,13 @@
 import health from '../app';
 
-test('calculate health', () => {
-  const wizard = health({ name: 'Маг', health: 90 });
-  const elf = health({ name: 'Эльф', health: 50 });
-  const dwarf = health({ name: 'Гном', health: 14 });
-
-  expect(wizard).toBe('healthy');
-  expect(elf).toBe('wounded');
-  expect(dwarf).toBe('critical');
-});
+test.each([
+  [{ name: 'Маг', health: 90 }, 'healthy'],
+  [{ name: 'Эльф', health: 50 }, 'wounded'],
+  [{ name: 'Гном', health: 14 }, 'critical'],
+])(
+  ('calculate health %s is %s'),
+  (obj, status) => {
+    const result = health(obj);
+    expect(result).toBe(status);
+  },
+);
